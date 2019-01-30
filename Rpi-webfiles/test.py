43b378e322018265030583f9b8caef7385257a25
pyRoot_walker <chiioleong519@gmail.com>
2019-01-30 20:25:04 +0800

    change data

--- Rpi-webfiles/test.py
+++ Rpi-webfiles/test.py
@@ -1,6 +1,20 @@
-import requests
+import cv2
 
-url = "http://192.168.170.112:4000/data"
-message = {"data": True}
+cap = cv2.VideoCapture(0)
 
-requests.post(url, data=message)
+face_cascade = cv2.CascadeClassifier("../pycharm/opencv-master/data/haarcascades/haarcascade_frontalface_default.xml")
+
+while True:
+    ret, frame = cap.read()
+    if ret:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        rects = face_cascade.detectMultiScale(gray, minSize=(150, 150))
+        for (x, y, w, h) in rects:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
