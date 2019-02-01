edefbe28890a7124cfa9e1270ab2c581296d8d90
RootadminWalker <chiioleong519@gmail.com>
2019-02-01 18:29:00 +0800

    test gpio prot

--- Rpi-webfiles/test.py
+++ Rpi-webfiles/test.py
@@ -1,20 +1,16 @@
-import cv2
+import RPi.GPIO as GPIO
+import time
 
-cap = cv2.VideoCapture(0)
+led = 4
+GPIO.setmode(GPIO.BCM)
 
-face_cascade = cv2.CascadeClassifier("../pycharm/opencv-master/data/haarcascades/haarcascade_frontalface_default.xml")
+GPIO.setup(led, GPIO.OUT)
 
-while True:
-    ret, frame = cap.read()
-    if ret:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        rects = face_cascade.detectMultiScale(gray, minSize=(150, 150))
-        for (x, y, w, h) in rects:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+i = 0
+while i <= 5:
+    GPIO.output(led, GPIO.HIGH)
+    time.sleep(2)
+    GPIO.output(led, GPIO.LOW)
+    i += 1
+GPIO.cleanup()
 
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(20) & 0xFF == ord('q'):
-            break
-
-cap.release()
-cv2.destroyAllWindows()
