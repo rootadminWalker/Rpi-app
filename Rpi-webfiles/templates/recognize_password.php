0390df23cc13f3ee28470c8462f972435c6f3207
pyRoot_walker <chiioleong519@gmail.com>
2019-02-06 16:03:22 +0800

    Changed the password recognize

--- Rpi-webfiles/templates/recognize_password.php
+++ Rpi-webfiles/templates/recognize_password.php
@@ -3,9 +3,8 @@
 	$user_password = $_POST["user_password"];
 	$_isTrue = false;
 	if ($user_password === $password) {
-		$_isTrue = true;
+		echo true;
 	} else {
-		$_isTrue = false;
+		echo false;
 	}
-	echo $_isTrue;
 ?>
