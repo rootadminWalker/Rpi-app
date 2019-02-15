a27347d121f0282c63ea717b73ddc72dc958fbb1
pyRoot_walker <chiioleong519@gmail.com>
2019-02-15 13:34:13 +0800

    change http://localhost to localhost

--- Rpi-webfiles/app.py
+++ Rpi-webfiles/app.py
@@ -65,7 +65,7 @@ def frame(cap):
 
 def send_image():
 	global user
-	URL = "http://localhost:4000"
+	URL = "localhost:4000"
 	files = {'media': open("/home/pi/workspace/Rpi-app/Rpi-webfiles/static/temp.jpg", "rb")}
 	req = requests.post(URL, files=files)
 	user = req.text
