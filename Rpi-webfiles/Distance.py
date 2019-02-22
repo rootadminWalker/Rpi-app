c59d8560ec5ab8acb638707fe21fa86d61c361f5
chiioleong519@gmail.com <chiioleong519@gmail.com>
2019-02-22 20:39:02 +0800

    modified distance py

--- Rpi-webfiles/Distance.py
+++ Rpi-webfiles/Distance.py
@@ -1,3 +1,4 @@
+#!/opt/miniconda3/bin/python
 import RPi.GPIO as gpio
 import time
 
@@ -9,20 +10,20 @@ echo = 18
 gpio.setup(trig, gpio.OUT)
 gpio.setup(echo, gpio.IN)
 
-gpio.output(trig, True)
-time.sleep(0.0001)
-gpio.output(trig, False)
-end, start = None, None
+while True:
+	gpio.output(trig, True)
+	time.sleep(1)
+	gpio.output(trig, False)
+	end, start = None, None
+	while not gpio.input(echo):
+		start = time.time()
 
-while not gpio.input(echo):
-    start = time.time()
+	while gpio.input(echo):
+	    end = time.time()
 
-while gpio.input(echo):
-    end = time.time()
+	sig_time = end - start
+	distance = sig_time / 0.000058
+	print('Detected distance: {}cm'.format(round(distance, 2)))
 
-sig_time = end - start
-distance = sig_time / 0.000058
-print('Detected distance: {}.cm'.format(distance))
-
-gpio.cleanp()
+gpio.cleanup()
 
