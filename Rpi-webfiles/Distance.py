2ebf61c30c24e39a614889f95c7e47d156730ed6
pyRoot_walker <chiioleong519@gmail.com>
2019-02-25 13:26:01 +0800

    catch error

--- Rpi-webfiles/Distance.py
+++ Rpi-webfiles/Distance.py
@@ -11,19 +11,21 @@ gpio.setwarnings(False)
 gpio.setup(trig, gpio.OUT)
 gpio.setup(echo, gpio.IN)
 
-while True:
-    gpio.output(trig, True)
-    time.sleep(0.0001)
-    gpio.output(trig, False)
-    end, start = None, None
+try:
+    while True:
+        gpio.output(trig, True)
+        time.sleep(0.0001)
+        gpio.output(trig, False)
+        end, start = None, None
 
-    while not gpio.input(echo):
-        start = time.time()
+        while not gpio.input(echo):
+            start = time.time()
 
-    while gpio.input(echo):
-        end = time.time()
+        while gpio.input(echo):
+            end = time.time()
 
-    sig_time = end - start
-    distance = sig_time / 0.000058
-    print('Detected distance: {}cm'.format(round(distance, 2)))
-
+        sig_time = end - start
+        distance = sig_time / 0.000058
+        print('Detected distance: {}cm'.format(round(distance, 2)))
+except KeyboardInterrupt:
+    gpio.cleanup()
