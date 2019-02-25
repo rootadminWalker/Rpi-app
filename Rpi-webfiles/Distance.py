45f6c6ea5158e29241719fe7a890496fa529bdcb
pyRoot_walker <chiioleong519@gmail.com>
2019-02-25 13:22:09 +0800

    Ping distance any time

--- Rpi-webfiles/Distance.py
+++ Rpi-webfiles/Distance.py
@@ -7,23 +7,25 @@ gpio.setmode(gpio.BCM)
 trig = 4
 echo = 18
 
+gpio.setwarnings(False)
 gpio.setup(trig, gpio.OUT)
 gpio.setup(echo, gpio.IN)
 
-gpio.output(trig, True)
-time.sleep(0.0001)
-gpio.output(trig, False)
-end, start = None, None
+while True:
+    gpio.output(trig, True)
+    time.sleep(0.0001)
+    gpio.output(trig, False)
+    end, start = None, None
 
-while not gpio.input(echo):
-    start = time.time()
+    while not gpio.input(echo):
+        start = time.time()
 
-while gpio.input(echo):
-    end = time.time()
+    while gpio.input(echo):
+        end = time.time()
 
-sig_time = end - start
-distance = sig_time / 0.000058
-print('Detected distance: {}cm'.format(round(distance, 2)))
+    sig_time = end - start
+    distance = sig_time / 0.000058
+    print('Detected distance: {}cm'.format(round(distance, 2)))
 
-gpio.cleanup()
+    gpio.cleanup()
 
