61937e8420bb57eac5adf366c48820f7f1c59c84
chiioleong519@gmail.com <chiioleong519@gmail.com>
2019-02-28 20:20:39 +0800

    enter test

--- Rpi-webfiles/test.py
+++ Rpi-webfiles/test.py
@@ -1,17 +1,5 @@
-from flask import *
-from flask_mail import *
+from getpass import getpass
 
-mail = Mail()
+password = getpass(prompt="Password:")
+print(password)
 
-app = Flask(__name__)
-app.config.from_pyfile('config.cfg')
-mail.init_app(app)
-
-@app.route("/")
-def index():
-    msg = Message("hello", sender="chiioleong519@gmail.com", recipients=["chiioleong519@gmail.com"])
-    mail.send(msg)
-    return "message sent"
-
-
-app.run(port=8080, debug=True)
