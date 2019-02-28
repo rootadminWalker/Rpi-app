f1c5d5a8f6c62b4c559ccf5b8bc1a702a5732208
pyRoot_walker <chiioleong519@gmail.com>
2019-02-28 16:04:00 +0800

    ping with function

--- Rpi-webfiles/Distance.py
+++ Rpi-webfiles/Distance.py
@@ -2,33 +2,35 @@
 import RPi.GPIO as gpio
 import time
 
-gpio.setmode(gpio.BCM)
 
-trig = 27
-echo = 17
+def ping_cm(trig=27, echo=17):
+    gpio.setmode(gpio.BCM)
 
-gpio.setwarnings(False)
-gpio.setup(trig, gpio.OUT)
-gpio.setup(echo, gpio.IN)
-end, start = None, None
+    gpio.setwarnings(False)
+    gpio.setup(trig, gpio.OUT)
+    gpio.setup(echo, gpio.IN)
+    end, start = None, None
 
-try:
-    while True:
-        gpio.output(trig, True)
-        time.sleep(0.0001)
-        gpio.output(trig, False)
+    try:
+        while True:
+            gpio.output(trig, True)
+            time.sleep(0.0001)
+            gpio.output(trig, False)
 
-        while not gpio.input(echo):
-            start = time.time()
-            if time.time() - start > 3:
-                break
+            while not gpio.input(echo):
+                start = time.time()
+                if time.time() - start > 3:
+                    break
 
-        while gpio.input(echo):
-            end = time.time()
-        if end is not None:
-            sig_time = end - start
-            distance = sig_time / 0.000058
-            print('Detected distance: {}cm'.format(round(distance, 2)))
-            time.sleep(1)
-except KeyboardInterrupt:
-    gpio.cleanup()
+            while gpio.input(echo):
+                end = time.time()
+            if end is not None:
+                sig_time = end - start
+                distance = sig_time / 0.000058
+                return distance
+    except KeyboardInterrupt:
+        gpio.cleanup()
+
+
+if __name__ == '__main__':
+    print(ping_cm())
