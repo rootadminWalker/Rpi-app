80ceb5931b5698150ab1411bf9f99f71f7fbdc9c
pyRoot_walker <chiioleong519@gmail.com>
2019-03-29 20:37:19 +0800

    show username at success page

--- Rpi-webfiles/app.py
+++ Rpi-webfiles/app.py
@@ -251,7 +251,7 @@ def no_forgot_password():
 @app.route("/borrow_success")
 def borrow_success():
 	global users
-	return render_template("borrow_success.html", user=users)
+	return render_template("borrow_success.html")
 
 
 @app.route("/enter_admin_password")
