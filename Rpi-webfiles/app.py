e09f05f45598941a7734316f172e6dc5efd9fe11
pyRoot_walker <chiioleong519@gmail.com>
2019-04-03 11:38:21 +0800

    Catch face detection error

--- Rpi-webfiles/app.py
+++ Rpi-webfiles/app.py
@@ -46,7 +46,7 @@ def frame(cap):
 	while True:
 		try:
 			_, frame = cap.read()
-			frame.copy
+			frame.copy()
 		except AttributeError:
 			_isError = True
 			_ErrorCameraMessage = "CAMERA_CONNECTION_ERROR"
